0630f513e6f2d1ee031bc2b937e87860444b8d42
Esun Kim <veblush@gmail.com>
2016-01-13 09:27:37 +0900

    Clean up CSharpCompilerFix code

--- core/RoslynCompilerFix/CSharpCompilerFix.cs
+++ core/RoslynCompilerFix/CSharpCompilerFix.cs
@@ -10,31 +10,31 @@ namespace RoslynCompilerFix
     {
         public static void Process(AssemblyDefinition assemblyDef)
         {
-            foreach (var moduleDef in assemblyDef.Modules)
+            foreach (var module in assemblyDef.Modules)
             {
-                foreach (var type in moduleDef.Types)
+                var generatedNames = module.GetType("Microsoft.CodeAnalysis.CSharp.Symbols.GeneratedNames");
+                if (generatedNames != null)
                 {
-                    if (type.FullName == "Microsoft.CodeAnalysis.CSharp.Symbols.GeneratedNames")
+                    foreach (var method in generatedNames.Methods)
                     {
-                        foreach (var method in type.Methods)
-                        {
-                            if (method.Name == "MakeHoistedLocalFieldName")
-                                Fix_GeneratedNames_MakeHoistedLocalFieldName(method);
-                            else if (method.Name == "MakeMethodScopedSynthesizedName")
-                                Fix_GeneratedNames_MakeMethodScopedSynthesizedName(method);
-                            else if (method.Name == "ThisProxyFieldName")
-                                Fix_GeneratedNames_ThisProxyFieldName(method);
-                            else if (method.Name == "MakeLambdaMethodName")
-                                Fix_GeneratedNames_MakeLambdaMethodName(method);
-                        }
+                        if (method.Name == "MakeHoistedLocalFieldName")
+                            Fix_GeneratedNames_MakeHoistedLocalFieldName(method);
+                        else if (method.Name == "MakeMethodScopedSynthesizedName")
+                            Fix_GeneratedNames_MakeMethodScopedSynthesizedName(method);
+                        else if (method.Name == "ThisProxyFieldName")
+                            Fix_GeneratedNames_ThisProxyFieldName(method);
+                        else if (method.Name == "MakeLambdaMethodName")
+                            Fix_GeneratedNames_MakeLambdaMethodName(method);
                     }
-                    else if (type.FullName == "Microsoft.CodeAnalysis.CSharp.LambdaFrame")
+                }
+                
+                var lambdaFrame = module.GetType("Microsoft.CodeAnalysis.CSharp.LambdaFrame");
+                if (lambdaFrame != null)
+                {
+                    foreach (var method in lambdaFrame.Methods)
                     {
-                        foreach (var method in type.Methods)
-                        {
-                            if (method.Name == ".ctor")
-                                Fix_LambdaFrame_Constructor(method);
-                        }
+                        if (method.Name == ".ctor")
+                            Fix_LambdaFrame_Constructor(method);
                     }
                 }
             }
@@ -44,8 +44,13 @@ namespace RoslynCompilerFix
         {
             // Goal:
             //   Rename <v>4__1 to <v>__1 for local variables in iterator
-            // How:
-            //   Remove following code from body of GeneratedNames.MakeHoistedLocalFieldName
+            //
+            // How for Roslyn:
+            //   Remove following code from GeneratedNames.MakeHoistedLocalFieldName
+            //     builder.Append((char)GeneratedNameKind.HoistedLocalField);
+            //
+            // How for IL:
+            //   Remove following code from body of method
             //     IL_003c: ldloc.0
             //     IL_003d: ldc.i4.s 53
             //     IL_003f: callvirt System.Text.StringBuilder System.Text.StringBuilder::Append(System.Char)
@@ -68,24 +73,33 @@ namespace RoslynCompilerFix
         {
             // Goal:
             //   Rename <TestCoroutine>d__1 to <TestCoroutine>c__Iterator0 for iterator class name
-            // How:
-            //   Append  following code from MakeMethodScopedSynthesizedName
-            //     IL_0033: ldloc.0
-            //     IL_0034: ldc.i4.s 62
-            //     IL_0036: callvirt instance class [System.Runtime]System.Text.StringBuilder [System.Runtime]System.Text.StringBuilder::Append(char)
-            //     IL_003b: pop
-            //     IL_003c: ldloc.0
-            //     IL_003d: ldarg.0
-            //     IL_003e: conv.u2
-            //     IL_003f: callvirt instance class [System.Runtime]System.Text.StringBuilder [System.Runtime]System.Text.StringBuilder::Append(char)
-            //     IL_0044: pop
-            //     >> IL_?: ldarg.0
-            //     >> IL_?: ldc.i4.s 100
-            //     >> IL_?: bne.un.s OUT
-            //     >> IL_?: ldloc.0
-            //     >> IL_?: ldstr "__Iterator"
-            //     >> IL_?: callvirt instance class [mscorlib]System.Text.StringBuilder [mscorlib]System.Text.StringBuilder::Append(string)
-            //     >> IL_?: pop
+            //
+            // How for Roslyn:
+            //   Append code following code at GeneratedNames.MakeMethodScopedSynthesizedName
+            //     builder.Append('>');
+            //     builder.Append((char)kind);
+            //     >> if (kind == GeneratedNameKind.StateMachineType) {
+            //     >>   builder.Append("__Iterator")
+            //     >> }
+            // 
+            // How for IL:
+            //   IL_0033: ldloc.0
+            //   IL_0034: ldc.i4.s 62
+            //   IL_0036: callvirt instance class [System.Runtime]System.Text.StringBuilder [System.Runtime]System.Text.StringBuilder::Append(char)
+            //   IL_003b: pop
+            //   IL_003c: ldloc.0
+            //   IL_003d: ldarg.0
+            //   IL_003e: conv.u2
+            //   IL_003f: callvirt instance class [System.Runtime]System.Text.StringBuilder [System.Runtime]System.Text.StringBuilder::Append(char)
+            //   IL_0044: pop
+            //   >> IL_?: ldarg.0
+            //   >> IL_?: ldc.i4.s IL_BASE
+            //   >> IL_?: bne.un.s OUT
+            //   >> IL_?: ldloc.0
+            //   >> IL_?: ldstr "__Iterator"
+            //   >> IL_?: callvirt instance class [mscorlib]System.Text.StringBuilder [mscorlib]System.Text.StringBuilder::Append(string)
+            //   >> IL_?: pop
+            //   IL_BASE:
 
             var il = method.Body.GetILProcessor();
             for (var i = 0; i < il.Body.Instructions.Count; i++)
@@ -93,20 +107,18 @@ namespace RoslynCompilerFix
                 var inst = il.Body.Instructions[i];
                 if (inst.OpCode.Code == Code.Ldc_I4_S && (sbyte)inst.Operand == 62)
                 {
-                    var baseInst = il.Body.Instructions[i + 7];
-                    var baseInst2 = il.Body.Instructions[i + 8];
-                    var appendCharRef = ((MethodReference)il.Body.Instructions[i + 1].Operand);
+                    var baseInst = il.Body.Instructions[i + 8];
 
                     var appendStringMethod = typeof(StringBuilder).GetMethod("Append", new Type[] { typeof(string) });
-                    var appendString = appendCharRef.DeclaringType.Module.Import(appendStringMethod);
+                    var appendString = method.Module.Import(appendStringMethod);
 
-                    il.InsertAfter(baseInst, il.Create(OpCodes.Pop));
-                    il.InsertAfter(baseInst, il.Create(OpCodes.Callvirt, appendString));
-                    il.InsertAfter(baseInst, il.Create(OpCodes.Ldstr, "__Iterator"));
-                    il.InsertAfter(baseInst, il.Create(OpCodes.Ldloc_0));
-                    il.InsertAfter(baseInst, il.Create(OpCodes.Bne_Un_S, baseInst2));
-                    il.InsertAfter(baseInst, il.Create(OpCodes.Ldc_I4_S, (sbyte)100));
-                    il.InsertAfter(baseInst, il.Create(OpCodes.Ldarg_0));
+                    il.InsertBefore(baseInst, il.Create(OpCodes.Ldarg_0));
+                    il.InsertBefore(baseInst, il.Create(OpCodes.Ldc_I4_S, (sbyte)100));
+                    il.InsertBefore(baseInst, il.Create(OpCodes.Bne_Un_S, baseInst));
+                    il.InsertBefore(baseInst, il.Create(OpCodes.Ldloc_0));
+                    il.InsertBefore(baseInst, il.Create(OpCodes.Ldstr, "__Iterator"));
+                    il.InsertBefore(baseInst, il.Create(OpCodes.Callvirt, appendString));
+                    il.InsertBefore(baseInst, il.Create(OpCodes.Pop));
                     break;
                 }
             }
@@ -116,7 +128,14 @@ namespace RoslynCompilerFix
         {
             // Goal:
             //   Rename <>4__this to <>f__this for this in iterator class
-            // How:
+            //
+            // How for Roslyn:
+            //   Modify return statement of GeneratedNames.ThisProxyFieldNam from
+            //     return "<>4__this";
+            //   to
+            //     return "<>f__this";
+            //
+            // How for IL:
             //   Change literal "<>4__this" to "<>f__this" at ThisProxyFieldName()
             //     IL_0000: ldstr "<>4__this"
             //     IL_0005: ret
@@ -142,24 +161,23 @@ namespace RoslynCompilerFix
             //   Modify constructor of LambdaFrame from
             //      LambdaFrame(MethodSymbol topLevelMethod, ...)
             //       : base(MakeName(...
-            //   To
+            //   to
             //      LambdaFrame(MethodSymbol topLevelMethod, ...)
             //       : base("<" + topLevelMethod.Name + ">c__AnonStorey" + MakeName(...
             //
             // How for IL:
-            //   Insert codes like:
-            //     IL_0000: ldarg.0
-            //     >> IL_?: ldstr "<"
-            //     >> IL_?: ldarg.1
-            //     >> IL_?: callvirt instance string Microsoft.CodeAnalysis.CSharp.Symbol::get_Name()
-            //     >> IL_?: ldstr ">c__AnonStorey"
-            //     IL_0001: ldarg.2
-            //     IL_0002: ldarg.3
-            //     IL_0003: ldarg.s closureId
-            //     IL_0005: call string Microsoft.CodeAnalysis.CSharp.LambdaFrame::MakeName(class Microsoft.CodeAnalysis.SyntaxNode, valuetype Microsoft.CodeAnalysis.CodeGen.DebugId, valuetype Microsoft.CodeAnalysis.CodeGen.DebugId)
-            //     >> IL_?: call string [mscorlib]System.String::Concat(string, string, string, string)
-            //     IL_000a: ldarg.1
-            //     IL_000b: call instance void Microsoft.CodeAnalysis.CSharp.Symbols.SynthesizedContainer::.ctor(string, class Microsoft.CodeAnalysis.CSharp.Symbols.MethodSymbol)
+            //   IL_0000: ldarg.0
+            //   >> IL_?: ldstr "<"
+            //   >> IL_?: ldarg.1
+            //   >> IL_?: callvirt instance string Microsoft.CodeAnalysis.CSharp.Symbol::get_Name()
+            //   >> IL_?: ldstr ">c__AnonStorey"
+            //   IL_0001: ldarg.2
+            //   IL_0002: ldarg.3
+            //   IL_0003: ldarg.s closureId
+            //   IL_0005: call string Microsoft.CodeAnalysis.CSharp.LambdaFrame::MakeName(class Microsoft.CodeAnalysis.SyntaxNode, valuetype Microsoft.CodeAnalysis.CodeGen.DebugId, valuetype Microsoft.CodeAnalysis.CodeGen.DebugId)
+            //   >> IL_?: call string [mscorlib]System.String::Concat(string, string, string, string)
+            //   IL_000a: ldarg.1
+            //   IL_000b: call instance void Microsoft.CodeAnalysis.CSharp.Symbols.SynthesizedContainer::.ctor(string, class Microsoft.CodeAnalysis.CSharp.Symbols.MethodSymbol)
 
             var il = method.Body.GetILProcessor();
             var baseInst = il.Body.Instructions[1];
@@ -188,7 +206,14 @@ namespace RoslynCompilerFix
         {
             // Goal:
             //   Rename <Start>b__0 to <>m__<Start>b__0 for method name in lambda class
-            // How:
+            //
+            // How for Roslyn:
+            //   Modify return statement of GeneratedNames.MakeLambdaMethodName from
+            //     return MakeMethodScopedSynthesizedName(GeneratedNameKind.LambdaMethod, ...
+            //   to 
+            //     return "<>m__" + MakeMethodScopedSynthesizedName(GeneratedNameKind.LambdaMethod, ...
+            //
+            // How for IL:
             //   >> IL_?: ldstr "<>m__"
             //   IL_0000: ldc.i4.s 57
             //   IL_0002: ldarg.0
