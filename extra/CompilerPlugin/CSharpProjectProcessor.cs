3664513345421663791580e046a2d5409cd27ed2
Esun Kim <veblush@gmail.com>
2016-03-14 23:19:56 +0900

    Add CSharpProjectProcessor to CompilerPlugin

---
+++ extra/CompilerPlugin/CSharpProjectProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Linq;
+using UnityEditor;
+
+public class CSharpProjectProcessor : AssetPostprocessor
+{
+	private static bool OnPreGeneratingCSProjectFiles()
+	{
+		var currentDirectory = Directory.GetCurrentDirectory();
+		var projectFiles = Directory.GetFiles(currentDirectory, "*.csproj");
+
+		foreach (var file in projectFiles)
+		{
+			UpdateProjectFile(file);
+		}
+
+		return false;
+	}
+
+	private static void UpdateProjectFile(string file)
+	{
+		var lines = File.ReadAllLines(file);
+		lines = lines.Where(line => line.Contains("LangVersion") == false).ToArray();
+		File.WriteAllLines(file, lines);
+	}
+}
