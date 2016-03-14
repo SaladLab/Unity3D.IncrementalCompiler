635a24d28717b8a9527092ee0ab141a1dfc842b3
Esun Kim <veblush@gmail.com>
2016-03-14 23:20:53 +0900

    Update build tools

--- build.cmd
+++ build.cmd
@@ -3,8 +3,8 @@
 pushd %~dp0
 
 tools\nuget\NuGet.exe update -self
-tools\nuget\NuGet.exe install FAKE -ConfigFile tools\nuget\Nuget.Config -OutputDirectory packages -ExcludeVersion -Version 4.12
-tools\nuget\NuGet.exe install ILRepack -ConfigFile tools\nuget\Nuget.Config -OutputDirectory packages -ExcludeVersion -Version 2.0.9
+tools\nuget\NuGet.exe install FAKE -ConfigFile tools\nuget\Nuget.Config -OutputDirectory packages -ExcludeVersion -Version 4.22
+tools\nuget\NuGet.exe install ILRepack -ConfigFile tools\nuget\Nuget.Config -OutputDirectory packages -ExcludeVersion -Version 2.0.10
 
 set encoding=utf-8
 packages\FAKE\tools\FAKE.exe build.fsx %*
