699f670886cf258fef09e9c36877675d3af916d7
Esun Kim <veblush@gmail.com>
2016-03-28 14:14:46 +0900

    Update FAKE 4.22.8

--- build.cmd
+++ build.cmd
@@ -3,7 +3,7 @@
 pushd %~dp0
 
 tools\nuget\NuGet.exe update -self
-tools\nuget\NuGet.exe install FAKE -ConfigFile tools\nuget\Nuget.Config -OutputDirectory packages -ExcludeVersion -Version 4.22
+tools\nuget\NuGet.exe install FAKE -ConfigFile tools\nuget\Nuget.Config -OutputDirectory packages -ExcludeVersion -Version 4.22.8
 tools\nuget\NuGet.exe install ILRepack -ConfigFile tools\nuget\Nuget.Config -OutputDirectory packages -ExcludeVersion -Version 2.0.10
 
 set encoding=utf-8
