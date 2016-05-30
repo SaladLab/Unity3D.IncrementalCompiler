74cf6af822e194eeb73bd475283d7c96b43d60de
Esun Kim <veblush@gmail.com>
2016-05-30 22:46:56 +0900

    Update FAKE.BuildLib 0.3.5 from 0.1.8

--- build.cmd
+++ build.cmd
@@ -6,16 +6,16 @@ SET PACKAGEPATH=.\packages\
 SET NUGET=.\tools\nuget\NuGet.exe
 SET NUGETOPTIONS=-ConfigFile .\tools\nuget\NuGet.Config -OutputDirectory %PACKAGEPATH% -ExcludeVersion
 
-IF NOT EXIST %PACKAGEPATH%FAKE\Ver_4.23.0 (
+IF NOT EXIST %PACKAGEPATH%FAKE\Ver_4.25.4 (
   RD /S/Q %PACKAGEPATH%FAKE
-  %NUGET% install FAKE -Version 4.23.0 %NUGETOPTIONS%
-  COPY NUL %PACKAGEPATH%FAKE\Ver_4.23.0
+  %NUGET% install FAKE -Version 4.25.4 %NUGETOPTIONS%
+  COPY NUL %PACKAGEPATH%FAKE\Ver_4.25.4
 )
 
-IF NOT EXIST %PACKAGEPATH%FAKE.BuildLib\Ver_0.1.8 (
+IF NOT EXIST %PACKAGEPATH%FAKE.BuildLib\Ver_0.3.5 (
   RD /S/Q %PACKAGEPATH%FAKE.BuildLib
-  %NUGET% install FAKE.BuildLib -Version 0.1.8 %NUGETOPTIONS%
-  COPY NUL %PACKAGEPATH%FAKE.BuildLib\Ver_0.1.8
+  %NUGET% install FAKE.BuildLib -Version 0.3.5 %NUGETOPTIONS%
+  COPY NUL %PACKAGEPATH%FAKE.BuildLib\Ver_0.3.5
 )
 
 set encoding=utf-8
