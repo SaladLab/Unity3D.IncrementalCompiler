9e63058c7f400578c02782ffa706bff6216fab6b
Esun Kim <veblush@gmail.com>
2016-06-06 20:47:52 +0900

    Remove warning.

--- core/UnityPackage/Assets/Editor/CompilerSettings.cs
+++ core/UnityPackage/Assets/Editor/CompilerSettings.cs
@@ -210,6 +210,7 @@ public class CompilerSettings : EditorWindow
         }
         catch (Exception e)
         {
+            Debug.LogWarning("GetUniversalCompilerLastBuildLogs:" + e);
         }
 
         return _ucLastBuildLog;
